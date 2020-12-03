52c482f8f7df9f8556c710a949df2351a772819f
PakoPortalo <PakoPortalo@users.noreply.github.com>
2020-12-03 09:29:45 +0100

    cube linux 2

--- check_map_clean.c
+++ check_map_clean.c
@@ -3,41 +3,68 @@
 /*                                                        :::      ::::::::   */
 /*   check_map_clean.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tamagotchi <tamagotchi@student.42.fr>      +#+  +:+       +#+        */
+/*   By: fportalo <fportalo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 10:18:04 by fportalo          #+#    #+#             */
-/*   Updated: 2020/12/02 17:42:35 by tamagotchi       ###   ########.fr       */
+/*   Updated: 2020/12/03 09:29:08 by fportalo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-// PROBLEMA EN FIND_ORIGIN CON EL STRCHAR, TENGO QUE HACER 4 IFS PORQUE SI NO NO ME FUNCIONA WTF NIGGA
+void	get_coordinates(int *x, int *y, mapstr *raw)
+{
+	if (!*x && !*y)
+	{
+		*x = raw->x;
+		*y = raw->y;
+	}
+}
+
+int	check_origin_errors(int i, int x, int y, mapstr *raw)
+{
+	if (i == 0)
+	{
+		printf("Error. There is no player. Map need an origin point (N, S, W, E)\n");
+		return (-1);
+	}
+	if (i > 1)
+	{
+		printf("Error. There is more than one player. Map need just one origin point \n");
+		return (-1);
+	}
+	raw->x = x;
+	raw->y = y;
+	return(1);
+}
 
 int	find_origin(mapstr *raw)
 {
 	int i;
+	int x;
+	int y;
 
 	i = 0;
-	while (raw->x < (int)ft_strlen(raw->map[raw->y]) && raw->y < (raw->rows - 1)) // NO FUNCIONA EL CASO DE QUE HAYAN 2 PUNTOS
+	x = 0;
+	y = 0;
+	while (raw->x < (int)ft_strlen(raw->map[raw->y]) && raw->y < (raw->rows - 1))
 	{
-		
 		while (raw->map[raw->y][raw->x])
 		{
-			printf("%c\n", raw->map[raw->y][raw->x]);
 			if ((ft_strchr("NSWE", raw->map[raw->y][raw->x])))
+			{
+				get_coordinates(&x, &y, raw);
 				i++;
+			}
 			raw->x++;
 		}
 		raw->y++;
 		raw->x = 0;
 	}
-	if (i = 0)
-		printf("Error. There is no player. Map need an origin point (N, S, W, E)\n");
-		return (-1);
-	if (i > 1)
-		return (-1);
-	if (i = 1) 
+	if (check_origin_errors(i, x, y, raw) == -1)
+		return(-1);
+	printf("%dx, %dy\n", raw->x, raw->y);
+	return (1);
 }
 
 int		flood_fill(mapstr *raw, int x, int y)
